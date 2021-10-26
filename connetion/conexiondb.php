7c271e0145f6488f006880147fb3bf684faee3df
durvin <dervin48@gmail.com>
2021-10-25 18:20:59 -0600

    Cambio Conexio

--- connetion/conexiondb.php
+++ connetion/conexiondb.php
@@ -1,8 +1,8 @@
 <?php 
 
-$servidor="mysql:dbname=riverapc;localhost:3307";
-$usuario="root";
-$password="root";
+$servidor="mysql:id17788761_riverapc;localhost:3307";
+$usuario="id17788761_root";
+$password="Jutiapa1232020@";
 
 try{
     $pdo= new PDO($servidor,$usuario,$password,array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8"));
